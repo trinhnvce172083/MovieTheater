c49aa002c953227842b0e298a7b53b2e4cb4bd25
Trần Quang Thuận <ThuanTQSE182998@fpt.edu.vn>
2025-06-11 15:44:49 +0700

    admin dashboard + movie management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
-// import ScrollToTopButton from "@/components/ScrollToTopButton";
-import BackTop from "antd/es/float-button/BackTop";
-
+import {ThemeProvider} from "@/components/providers/ThemeProvider";
+import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,14 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          {/* <header className="fixed top-0 left-0 right-0 z-50">
-            <Header />
-          </header> */}
-          <main className="min-h-screen pt-24">{children}</main>
-          {/* <footer className="mt-8">
-            <Footer />
-          </footer> */}
-          <BackTop duration={200} visibilityHeight={50} />
+        <ThemeProvider 
+        attribute="class" 
+        defaultTheme="system" 
+        enableSystem
+        disableTransitionOnChange
+        >
+          <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { SearchOutlined, GlobalOutlined } from "@ant-design/icons";
@@ -12,10 +13,37 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import ROUTES from "@/constants/routes";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Sun, Moon } from "lucide-react";
 
 export default function Header() {
+  const [show, setShow] = useState(true);
+  const lastScrollY = useRef(0);
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY.current && currentScrollY > 60) {
+        setShow(false); // Kéo xuống thì ẩn
+      } else {
+        setShow(true); // Kéo lên thì hiện
+      }
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="flex items-center justify-between px-6 py-0 bg-black text-white relative z-10">
+    <header
+      className={`flex items-center justify-between px-6 py-0 bg-black text-white relative z-10 transition-transform duration-300 ${
+        show ? "translate-y-0" : "-translate-y-full"
+      }`}
+      style={{ position: "sticky", top: 0 }}
+    >
       <div className="flex items-center">
         <Link href={ROUTES.HOME} className="mr-8">
           <Image
@@ -49,10 +77,35 @@ export default function Header() {
         </nav>
       </div>
 
-      <div className="flex items-center space-x-6">
-        <button className="text-white hover:text-red-500 transition-colors pr-5">
+      <div className="flex items-center gap-x-6">
+        <button className="text-white hover:text-red-500 transition-colors pr-2">
           <SearchOutlined style={{ fontSize: "20px" }} />
         </button>
+        {/* Nút chọn theme */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="relative bg-transparent border-none hover:bg-gray-800/60 flex items-center justify-center p-1"
+            >
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Link
           href={ROUTES.LOGIN}
           className="rounded-full bg-red-500 hover:bg-blue-500 text-white px-4 py-1.5 font-medium transition-colors shadow-md"
