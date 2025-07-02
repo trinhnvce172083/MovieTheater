1dfa4b4fe4a15d4fc0670950ea7863b5a49dff1d
Ngo Viet Trinh <trinhnvce172083@fpt.edu.vn>
2025-07-02 17:00:02 +0700

    oke CornChip

--- src/app/booking/CornChip/page.tsx
+++ src/app/booking/CornChip/page.tsx
@@ -94,7 +94,7 @@ export default function CornChipPage() {
   return (
     <>
       <Header />
-      <div className="bg-[#151a23] text-white min-h-screen px-8 pt-2 mt-10">
+      <div className="bg-[#151a23] text-white min-h-screen px-8 pt-2">
         <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8 bg-[#151a23] rounded-xl">
           <ConcessionsList
             concessions={concessions}
