39ecdf12f22464d8ba1253b80a6082011f5dce41
Ngô Việt Trinh <trinhnvce172083@fpt.edu.vn>
2025-07-23 06:39:06 +0700

    fix CornChip

--- src/app/booking/CornChip/OrderSummary.tsx
+++ src/app/booking/CornChip/OrderSummary.tsx
@@ -36,12 +36,12 @@ const OrderSummary = ({ movieDetails, totalOrder, bookingData, concessions, quan
       </div>
       {/* Danh sách món đã chọn */}
       <div className="mb-2">
-        {concessions.filter(item => quantities[item.id] > 0).length > 0 ? (
-          concessions.filter(item => quantities[item.id] > 0).map(item => (
-            <div key={item.id} className="flex items-center gap-2 mb-2">
+        {concessions.filter(item => quantities[item.concessionId] > 0).length > 0 ? (
+          concessions.filter(item => quantities[item.concessionId] > 0).map(item => (
+            <div key={item.concessionId} className="flex items-center gap-2 mb-2">
               <img src={item.imageUrl || '/popcorn.jpg'} alt={item.name} className="w-8 h-8 object-cover rounded bg-white border" />
-              <span className="flex-1">{item.name} x {quantities[item.id]}</span>
-              <span>{(item.price * quantities[item.id]).toLocaleString()} VND</span>
+              <span className="flex-1">{item.name} x {quantities[item.concessionId]}</span>
+              <span>{(item.price * quantities[item.concessionId]).toLocaleString()} VND</span>
             </div>
           ))
         ) : (
